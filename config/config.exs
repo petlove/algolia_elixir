3b89a3cc4796ce054ef6f980b009da9716473e22
Marcio Toze <marcio.toze@gmail.com>
2021-12-27 00:46:06 -0300

    feat: parse filter map to string

--- config/config.exs
+++ config/config.exs
@@ -1,3 +1,5 @@
 import Config
 
+config :tesla, adapter: {Tesla.Adapter.Hackney, [path_encode_fun: &URI.encode/1]}
+
 import_config "#{config_env()}.exs"
--- lib/algolia_elixir/resources/search.ex
+++ lib/algolia_elixir/resources/search.ex
@@ -10,10 +10,39 @@ defmodule AlgoliaElixir.Resources.Search do
   end
 
   defp format_multi_queries(queries) do
-    Enum.map(queries, fn query ->
-      params = URI.encode_query(query[:params])
+    Enum.map(queries, fn %{params: params} = query ->
+      formated_params = format_params(params)
 
-      Map.put(query, :params, params)
+      Map.put(query, :params, formated_params)
     end)
+  end
+
+  defp format_params(%{filters: filters} = params) do
+    formated_filters = format_filters(filters)
+
+    params
+    |> Map.put(:filters, formated_filters)
+    |> URI.encode_query()
+  end
+
+  defp format_filters(filters) when is_binary(filters), do: filters
+
+  defp format_filters(filters) when is_map(filters) do
+    filters
+    |> Enum.reduce([], fn {name, values}, acc ->
+      facet =
+        cond do
+          is_binary(values) ->
+            "#{name}:#{values}"
+
+          is_list(values) ->
+            Enum.map_join(values, " OR ", fn value ->
+              "#{name}:#{value}"
+            end)
+        end
+
+      acc ++ ["(#{facet})"]
+    end)
+    |> Enum.join(" AND ")
   end
 end
