cc0f362d6127a27c6648c03c2b4f3fc94c40838b
Julia Litvinoff Justus <julialiff@hotmail.com>
2024-08-22 16:30:51 -0300

    failure tests

--- test/resources/rules_test.exs
+++ test/resources/rules_test.exs
@@ -15,7 +15,17 @@ defmodule AlgoliaElixirTest.Resources.RulesTest do
     assert {:ok, %{status: 200, body: ^rules}} = Rules.search_rules("my_index")
   end
 
-  test "batch rules" do
+  test "search rules failure" do
+    response = %{"message" => "indexName is not valid", "status" => 400}
+
+    mock(fn %{method: :post, url: "https://test-dsn.algolia.net/1/indexes//rules/search"} ->
+      json(response)
+    end)
+
+    assert {:ok, %{status: 200, body: ^response}} = Rules.search_rules("")
+  end
+
+  test "batch rules success" do
     rules = build_list(5, :rule)
     response = %{"taskID" => 1_718_439_032_100, "updatedAt" => "2024-08-22T16:24:03.079Z"}
 
@@ -24,5 +34,18 @@ defmodule AlgoliaElixirTest.Resources.RulesTest do
     end)
 
     assert {:ok, %{status: 200, body: ^response}} = Rules.batch_rules("my_index", rules)
+  end
+
+  test "batch rules failure" do
+    response = %{
+      "message" => "Missing mandatory attribute `objectID` (in `[0]`) (near 1:4)",
+      "status" => 400
+    }
+
+    mock(fn %{method: :post, url: "https://test-dsn.algolia.net/1/indexes/my_index/rules/batch"} ->
+      json(response)
+    end)
+
+    assert {:ok, %{status: 200, body: ^response}} = Rules.batch_rules("my_index", [])
   end
 end
